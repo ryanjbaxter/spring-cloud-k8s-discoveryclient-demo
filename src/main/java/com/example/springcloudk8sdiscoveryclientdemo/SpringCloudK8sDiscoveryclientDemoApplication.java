4ade66002b6efdb8d9810c64060bfb2ebaef0211
Ryan Baxter <rbaxter@vmware.com>
2022-02-20 19:59:32 -0500

    initial commit

---
+++ src/main/java/com/example/springcloudk8sdiscoveryclientdemo/SpringCloudK8sDiscoveryclientDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcloudk8sdiscoveryclientdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudK8sDiscoveryclientDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudK8sDiscoveryclientDemoApplication.class, args);
+	}
+
+}
