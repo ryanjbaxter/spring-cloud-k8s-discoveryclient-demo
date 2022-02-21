5d048eb74105512eada3dcd6f3e4a8746c93c7a6
Ryan Baxter <rbaxter@vmware.com>
2022-02-20 20:01:31 -0500

    adding rest controller

--- src/main/java/com/example/springcloudk8sdiscoveryclientdemo/SpringCloudK8sDiscoveryclientDemoApplication.java
+++ src/main/java/com/example/springcloudk8sdiscoveryclientdemo/SpringCloudK8sDiscoveryclientDemoApplication.java
@@ -2,12 +2,20 @@ package com.example.springcloudk8sdiscoveryclientdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringCloudK8sDiscoveryclientDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudK8sDiscoveryclientDemoApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String index() {
+		return "Hello Tanzu";
 	}
 
 }
